09c8a7b4569ab8f32198f69df57d11ea06368748
niqingyang <niqy@qq.com>
2019-06-19 14:35:55 +0800

    更新mobx-loading版本，修复Locale的Bug

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ export {default as app} from './app';
 export {default as stores} from './stores';
 export {default as dynamic} from './dynamic';
 export {default as namespace} from './namespace';
-export {default as Local} from './locale';
+export {default as Locale} from './locale';
 export {loadingStore, loading} from 'mobx-loading';
 export {default as renderRoutes, formatterRoutes} from './renderRoutes';
